699f0a5bbb38046d73588972bb7f4d279bae4ebd
Shubham <shubhambhutada7810@gmail.com>
2023-12-25 01:27:19 +0530

    again commited

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,15 +1,25 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 
 const FetchData = () => {
     const [userlist, setUserlist] = useState([])
 
+    // useEffect(() => {
+    //     axios.get("https://reqres.in/api/users")
+    //     .then(response => {
+    //         console.log(response.data.data)
+    //         setUserlist(response.data.data)
+    //     }) 
+    //     .catch(err => console.log(err)) 
+    // })
+
     function fetchusers() {
         axios.get("https://reqres.in/api/users")
         .then(response => {
             console.log(response.data.data)
-            setUserlist(response.data.data)}) 
-        .catch(err => console.log(err)) 
+            setUserlist(response.data.data)
+        }) 
+        .catch(err => console.log(err))  
     }
   return (
     <div>
@@ -25,15 +35,17 @@ const FetchData = () => {
                 </tr>
             </thead>
             <tbody>
-                {userlist.length===0 ? <tr><td>No data found to display</td></tr>:
-                (userlist.map(item => (
-                    <tr>
-                        <td>{item.first_name}</td>
-                        <td>{item.last_name}</td>
-                        <td>{item.email}</td>
-                        <td><img src={item.avatar} alt={item.first_name+" "+item.last_name}/></td>
-                    </tr>
-                )))} 
+                {
+                    userlist.length===0 ? (<tr> <td>No data found to display.</td></tr>):
+                    (userlist.map(item => (
+                        <tr>
+                            <td>{item.first_name}</td>
+                            <td>{item.last_name}</td>
+                            <td>{item.email}</td>
+                            <td><img src={item.avatar} alt={item.first_name+" "+item.last_name}/></td>
+                        </tr>
+                    )))
+                } 
             </tbody>
         </table>
     </div>
